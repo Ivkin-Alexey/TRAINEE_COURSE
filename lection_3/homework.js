061bb10d188089480907fc0071e4d4ebd7f6fdf5
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-06-11 17:39:31 +0300

    feat: Complete homework 3

--- lection_3/homework.js
+++ lection_3/homework.js
@@ -1,4 +1,4 @@
-// //1
+// // //1
 // const user = {
 //     name: 'Bob',
 //     funcFunc() {
@@ -23,10 +23,14 @@
 //     },
 // };
 //
-// user.funcFunc()(); // ?
-// user.funcArrow()(); // ?
-// user.arrowFunc()(); // ?
-// user.arrowArrow()(); // ?
+// user.funcFunc()(); // В браузере глобальный объект window, в NodeJS объект global
+// user.funcArrow()(); // В браузере и NodeJS объект user, поскольку у стрелочных функций лексический контекст, т.е. их
+// this зависит от контекста, где они были вызваны. This берется из ближайшего контекста выполнения. Т.е. стрелочная
+// функция использует this метода funcArrow
+// user.arrowFunc()(); // В браузере глобальный объект window, в NodeJS объект global, т.к. вызываемая функция
+// использует this стрелочной функции, у которой this ссылается на вышестоящий контекст выполнения, т.е. глобальный
+// объект
+// user.arrowArrow()(); //В браузере глобальный объект window, в NodeJs {}
 
 // 2
 // var poke1 = {name:'Pikachu'};
@@ -35,8 +39,10 @@
 //
 // var sayName = function(){ console.log(this.name) }
 //
-// sayName.bind(poke1).bind(poke2).call(poke3);
-
+// sayName.bind(poke1).bind(poke2).call(poke3); // Pikachu - поскольку методом bind устанавливается poke1 в качестве
+// контекста функции sayName. Второй метод bind игнорируется. Вызов функции происходит с помощью метода call, bind
+// функцию не вызывает, а возвращают новую функию со значением this, который равен первому аргументу функции bind.
+// вызов call не меняет this функции sayName, а только вызывает функцию sayName с уже установленным this как poke1
 
 // 3
 // const obj = {
@@ -51,36 +57,47 @@
 //         console.log(this.lastName);
 //     }
 // }
+
+// obj.showFirstName(); // Bill - потому что this здесь - это объект до точки, т.е. obj
+// obj.showLastName(); // undefined - из-за того, что showLastName стрелочная функция, а у стрелочных функций this
+// // указывает на вышестоящий контекст выполнения, т.е. по сути вызывается console.log(window.lastName) или
+// // console.log(global.lastName), а у этих объектов нет свойства lastName
 //
-// obj.showFirstName(); // ?
-// obj.showLastName(); // ?=
+// obj.showFirstName.bind({ firstName: 'Boris' })(); // Boris - потому что мы вызываем новую функцию, возвращаемую bind, у
+// // которой this указывает на объект, переданный в первый аргумент bind
 //
-// obj.showFirstName.bind({ firstName: 'Boris' })(); // ?
-// obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // ?
+// obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // Boris - всё, как в предыдущем случае
+// // поскольку второй bind игнорируется
 //
-// obj.showLastName.bind({ lastName: 'Boris' })(); // ?
+// obj.showLastName.bind({ lastName: 'Boris' })(); // Undefined, поскольку для стрелочных функций нельзя переопределить
+// // this, оно всегда будет определяться контекстом выполнения
 
 // 4
 
-// const user = {
-//     name: 'Mike',
-//     fn: function () {
-//         console.log(this.name)
-//     }
-// }
-//
-// setTimeout(user.fn, 1000)
+/*const user = {
+    name: 'Mike',
+    fn: function () {
+        console.log(this.name)
+    }
+}
 
+const fn = user.fn.bind(user);
 
+setTimeout(fn, 1000);*/
 
 // Что будет выведено в консоль после истечения таймаута и почему?
-// Сделайте так, чтоб починить и выводило "Mike"
+// Будет выведено undefined. Потому что при передаче метода отдельно от объекта происходит потеря контекста.
 
+// Сделайте так, чтоб починить и выводило "Mike"
+// Для этого можно привязать контектс с помощью метода bind.
 // Подсказка - ответ найдете в 5 ссылке README
 
 // 5
 //Исправьте cтроку(***), чтобы всё работало (других строк изменять не надо, кроме password, чтобы проверить if else).
-//
+// Как и в предыдущем случае, необходимо каждому методу, взятому из объекта user, указать контекст, иначе произойдет его
+// потеря. Сделать это возможно с помощью метода bind, потому что он возвращает привязанную функцию, this которой
+// установлен как первый аргумент метода bind. Также для предотвращения потери контекста возможно использовать функции
+// обертки, благодаря чему объект user замыкается внутри таких функций.
 // function askPassword(ok, fail) {
 //   let password = 'rockstar2'
 //   if (password == "rockstar") ok();
@@ -100,7 +117,8 @@
 //
 // };
 //
-// askPassword(user.loginOk, user.loginFail) //***;
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user)) //***;
+// askPassword(() => user.loginOk(), () => user.loginFail()) //***;
 
 
 
